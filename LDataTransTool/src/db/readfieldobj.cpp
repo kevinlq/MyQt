8d7a4c0567a0fc708d94195a371e1a296d5d1a2b
kevinlq0912 <kevinlq0912@163.com>
2017-06-14 18:01:58 +0800

    add

---
+++ LDataTransTool/src/db/readfieldobj.cpp
@@ -0,0 +1,6 @@
+#include "readfieldobj.h"
+
+ReadFieldObj::ReadFieldObj(QObject *parent) : QObject(parent)
+{
+
+}
---
+++ LDataTransTool/src/db/readfieldobj.h
@@ -0,0 +1,17 @@
+#ifndef READFIELDOBJ_H
+#define READFIELDOBJ_H
+
+#include <QObject>
+
+class ReadFieldObj : public QObject
+{
+    Q_OBJECT
+public:
+    explicit ReadFieldObj(QObject *parent = 0);
+
+signals:
+
+public slots:
+};
+
+#endif // READFIELDOBJ_H
